0d6bc79e110a179cfaa0c57865b3c4fe1489a51a
Carlos Martins <141359858+CMBMartins@users.noreply.github.com>
2026-04-10 10:24:04 -0300

    Update DespesaRepository.java

--- src/main/java/com/SpringBoot/projetosspringboot/DespesaRepository.java
+++ src/main/java/com/SpringBoot/projetosspringboot/DespesaRepository.java
@@ -2,6 +2,7 @@ package com.SpringBoot.projetosspringboot;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Map;
 
 public interface DespesaRepository extends JpaRepository<Despesa, Integer> {
 
@@ -10,5 +11,26 @@ public interface DespesaRepository extends JpaRepository<Despesa, Integer> {
     List<Despesa> findByUsuario(String usuario);
 
     List<Despesa> findByUsuarioAndDescricaoContainingIgnoreCase(String usuario, String descricao);
+
+@Query("SELECT SUM(d.valor) FROM Despesa d WHERE d.usuario = :usuario AND d.tipo = :tipo")
+Double sumByUsuarioETipo(String usuario, String tipo);
+
+
+@Query("""
+SELECT SUM(d.valor)
+FROM Despesa d
+WHERE d.usuario = :usuario
+AND EXTRACT(MONTH FROM d.data) = EXTRACT(MONTH FROM CURRENT_DATE)
+""")
+Double totalMesAtual(String usuario);
+
+
+@Query("""
+SELECT d.status, COUNT(d)
+FROM Despesa d
+WHERE d.usuario = :usuario
+GROUP BY d.status
+""")
+List<Object[]> contarStatusRaw(String usuario);
 
 }
