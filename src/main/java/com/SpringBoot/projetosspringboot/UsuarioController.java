3fef584bde39aec6d43ac9e24648b4d4e0063dd7
Carlos Martins <141359858+CMBMartins@users.noreply.github.com>
2026-04-11 16:13:29 -0300

    Change request mapping from '/usuarios' to '/usuario'

--- src/main/java/com/SpringBoot/projetosspringboot/UsuarioController.java
+++ src/main/java/com/SpringBoot/projetosspringboot/UsuarioController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping("/usuario")
 @CrossOrigin(origins = "*")
 public class UsuarioController {
 
