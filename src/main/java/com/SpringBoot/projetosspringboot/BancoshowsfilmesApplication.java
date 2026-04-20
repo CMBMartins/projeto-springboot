ac83be3f606b286d5f19550a4a45eebf9b86a6af
Carlos Martins <141359858+CMBMartins@users.noreply.github.com>
2026-04-20 11:20:48 -0300

    Update BancoshowsfilmesApplication.java

--- src/main/java/com/SpringBoot/projetosspringboot/BancoshowsfilmesApplication.java
+++ src/main/java/com/SpringBoot/projetosspringboot/BancoshowsfilmesApplication.java
@@ -1,5 +1,4 @@
-package com.bancoshows.bancoshowsfilmes;
-
+package com.SpringBoot.projetosspringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
