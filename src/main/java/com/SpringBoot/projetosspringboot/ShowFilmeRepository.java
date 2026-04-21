c48680f4bdb09464f5b475c2486dea93a970dc11
Carlos Martins <141359858+CMBMartins@users.noreply.github.com>
2026-04-21 08:09:07 -0300

    Add files via upload

--- src/main/java/com/SpringBoot/projetosspringboot/ShowFilmeRepository.java
+++ src/main/java/com/SpringBoot/projetosspringboot/ShowFilmeRepository.java
@@ -1,8 +1,9 @@
-package com.SpringBoot.projetosspringboot;
+package com.bancoshows.bancoshowsfilmes;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ShowFilmeRepository extends JpaRepository<BancoShowFilmes, Long> {
     List<BancoShowFilmes> findByArtistaContainingIgnoreCase(String artista);
-    List<BancoShowFilmes> findByUsuario(String usuario); 
+    List<BancoShowFilmes> findByUsuario(String usuario);   
 }
