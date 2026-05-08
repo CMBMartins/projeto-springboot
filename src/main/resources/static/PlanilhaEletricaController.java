122d9f88c1a7478a637fef26773e5a7b73761d2b
Carlos Martins <141359858+CMBMartins@users.noreply.github.com>
2026-05-08 10:35:57 -0300

    Add files via upload

---
+++ src/main/resources/static/PlanilhaEletricaController.java
@@ -0,0 +1,116 @@
+package com.SpringBoot.projetosspringboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/planilha")
+@CrossOrigin(origins = "*")
+public class PlanilhaEletricaController {
+
+    @Autowired
+    private PlanilhaEletricaRepository repository;
+
+    // Método Salvar
+    @PostMapping
+    public PlanilhaEletrica salvar(@RequestBody PlanilhaEletrica novo) {
+
+        double pt = novo.getPotenciatotal();
+        double tensao = 127;
+
+        double pc = pt * 0.66;
+        double ic = pt / tensao;
+        double ib = ic * 1.42;
+
+        novo.setPotenciacorrigida(arred(pc));
+        novo.setCorrenteic(arred(ic));
+        novo.setCorrenteib(arred(ib));
+
+        String tipo = novo.getTipocircuito();
+
+        if ("Tomada".equalsIgnoreCase(tipo)) {
+
+            if (ib <= 20) {
+                novo.setCondutor("2,5 mm²");
+                novo.setDisjuntor("20A");
+            } else if (ic <= 28) {
+                novo.setCondutor("4 mm²");
+                novo.setDisjuntor("25A");
+            } else {
+                novo.setCondutor("6 mm²");
+                novo.setDisjuntor("32A");
+            }
+
+            if ("Motor".equalsIgnoreCase(tipo)) {
+
+                if (ib <= 10) {
+                    novo.setCondutor("2,5 mm²");
+                    novo.setDisjuntor("10A");
+                } else if (ic <= 20) {
+                    novo.setCondutor("4 mm²");
+                    novo.setDisjuntor("20A");
+                } else {
+                    novo.setCondutor("6 mm²");
+                    novo.setDisjuntor("32A");
+                }
+            }
+
+        } else if ("Iluminacao".equalsIgnoreCase(tipo)) {
+
+            novo.setCondutor("1,5 mm²");
+            novo.setDisjuntor("10A");
+        }
+
+        return repository.save(novo);
+    }
+
+    // Método Editar
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@PathVariable Long id,
+            @RequestBody PlanilhaEletrica dados) {
+
+        return repository.findById(id)
+                .map(registro -> {
+
+                    registro.setProjeto(dados.getProjeto());
+                    registro.setCircuito(dados.getCircuito());
+                    registro.setDescricao(dados.getDescricao());
+                    registro.setPotenciatotal(dados.getPotenciatotal());
+                    registro.setPotenciacorrigida(dados.getPotenciacorrigida());
+                    registro.setTipocircuito(dados.getTipocircuito());
+                    registro.setCorrenteic(dados.getCorrenteic());
+                    registro.setCorrenteib(dados.getCorrenteib());
+                    registro.setDisjuntor(dados.getDisjuntor());
+                    registro.setCondutor(dados.getCondutor());
+                    registro.setUsuario(dados.getUsuario());
+
+                    repository.save(registro);
+
+                    return ResponseEntity.ok("Atualizado com sucesso");
+
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public List<PlanilhaEletrica> listar() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/buscar")
+    public List<PlanilhaEletrica> buscarPorCliente(@RequestParam String circuito) {
+        return repository.findByCircuitoContainingIgnoreCase(circuito);
+    }
+
+    @GetMapping("/usuario")
+    public List<PlanilhaEletrica> listarPorUsuario(@RequestParam String usuario) {
+        return repository.findByUsuario(usuario);
+    }
+
+    private double arred(double valor) {
+        return Math.round(valor * 100.0) / 100.0;
+    }
+}
